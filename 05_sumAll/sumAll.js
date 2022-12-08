b02702419fb185bc39b16c59da8af1d9c32588c2
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-08 11:29:14 -0700

    Complete problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+function isValid(number) {
+  const isNumber = typeof number === 'number';
+  const isPositive = number >= 0;
 
+  return isNumber && isPositive;
+}
+
+const sumAll = function(from, to) {
+  if (!isValid(from) || !isValid(to)) return 'ERROR';
+
+  const min = Math.min(from, to);
+  const max = Math.max(from, to);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
