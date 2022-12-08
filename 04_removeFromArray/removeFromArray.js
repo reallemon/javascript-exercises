dd2063b5bd3feadf38e1c42b31ba15928131608b
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-08 11:13:09 -0700

    Finish problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+  for (let value of values) {
+    const idx = array.indexOf(value);
+    if (idx >= 0) array.splice(idx, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
