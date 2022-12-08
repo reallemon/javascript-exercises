92a82219d9a31c1d4e462b3d6ad40289ba7bfe2b
Spencer Penrod <spencerpenrod@gmail.com>
2022-12-08 12:40:42 -0700

    Finish problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function(farenheit) {
+  return round((farenheit - 32) * (5/9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return round((celcius * (9/5) + 32));
 };
 
 // Do not edit below this line
